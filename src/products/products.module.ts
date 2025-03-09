5325b10f0892d8671623c45c3d41f2db9139a386
NourSoltan <nour.meghit@yassir.com>
2025-03-09 15:22:14 +0100

    Add module products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  controllers: [ProductsController]
+  controllers: [ProductsController],
+  imports: [ProductsModule]
 })
 export class ProductsModule {}
