cf66cd23ff25622b32b80480bbc2cf086d48d74a
Nour Meghit <75456170+NourSoltanaMEGHITyassir@users.noreply.github.com>
2025-07-04 13:29:27 +0100

    Squach (#14)
    * Add Stest Service
    
    * Add controller
    
    * Add Stest Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthController } from './auth/auth.controller';
 import { TestService } from './test/test.service';
+import { StestController } from './stest/stest.controller';
+import { StestService } from './stest/stest.service';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, TestService],
+  controllers: [AppController, AuthController, StestController],
+  providers: [AppService, TestService,StestService],
 })
 export class AppModule {}
