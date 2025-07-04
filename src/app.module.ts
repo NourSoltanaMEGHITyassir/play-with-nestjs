d69c283c0de6d4d6e2246f80d7755c78666b303a
Nour Meghit <75456170+NourSoltanaMEGHITyassir@users.noreply.github.com>
2025-07-04 11:52:34 +0100

    add user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthController } from './auth/auth.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController, AuthController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
