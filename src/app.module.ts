9d93a6a7c6f118b912569a30597298ec0b0fba6c
Nour Meghit <75456170+NourSoltanaMEGHITyassir@users.noreply.github.com>
2025-07-04 13:19:48 +0100

    Add Stest Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ProductsModule } from './products/products.module';
 import { AuthController } from './auth/auth.controller';
 import { TestService } from './test/test.service';
 import { StestController } from './stest/stest.controller';
+import { StestService } from './stest/stest.service';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController, AuthController, StestController],
-  providers: [AppService, TestService],
+  providers: [AppService, TestService,StestService],
 })
 export class AppModule {}
