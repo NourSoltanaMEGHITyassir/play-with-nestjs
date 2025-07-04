829c4ea206d7e3a7ef3e4acb2be157982fdb32d5
Nour Meghit <75456170+NourSoltanaMEGHITyassir@users.noreply.github.com>
2025-07-04 13:20:37 +0100

    Add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthController } from './auth/auth.controller';
 import { TestService } from './test/test.service';
+import { StestController } from './stest/stest.controller';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController, AuthController],
+  controllers: [AppController, AuthController, StestController],
   providers: [AppService, TestService],
 })
 export class AppModule {}
